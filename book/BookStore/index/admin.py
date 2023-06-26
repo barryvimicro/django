81e397d84b7da02fd686329b3aa9c377412f6a4f
barryvimicro <guoquan@vimicro.com>
2023-06-26 23:08:00 +0800

    上传book第一次

---
+++ book/BookStore/index/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin #Django自动在admin.py文件中导入
+from index.models import Book, Author,UserInfo #这个需要我们自己导入相应的模型类（数据表）
+admin.site.register([Book,Author,UserInfo])
